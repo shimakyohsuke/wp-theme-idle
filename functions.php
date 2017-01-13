2f3d69d908885f90682e47f29dd6223f7aebbce8
shimakyohsuke <shimakyohsuke@gmail.com>
2017-01-13 21:50:14 +0900

    Modify functions.php

--- functions.php
+++ functions.php
@@ -109,6 +109,8 @@ function idle_scripts() {
 
 	wp_enqueue_script( 'idle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+	wp_enqueue_script( 'idle-bundle', get_template_directory_uri() . '/bundle.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'idle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
